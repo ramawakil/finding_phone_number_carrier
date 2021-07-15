dccd633ec6e1be0d36de6a52e38b83d2f6fdc435
Marijani <ramadhanwakil19@gmail.com>
2021-07-15 12:48:00 +0300

    enjoy the immidiate release

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # finding_phone_number_carrier and some more things 
 find phone numbers carriers around the world with (pip phonenumbers)
+
+
+#sample output
+#   phone number yako pls:: 0684208765
+ #   Airtel
+
--- phone_number.py
+++ phone_number.py
@@ -2,5 +2,5 @@ import phonenumbers
 from phonenumbers import carrier
 
 
-number = phonenumbers.parse(input("phone number yako pls: "), None)
+number = phonenumbers.parse(f'+255{input("phone number yako pls:: ")}', None)
 print(carrier.name_for_number(number, lang="en"))
